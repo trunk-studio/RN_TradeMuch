4f381c6e362359202d7286e92da917de6239f02b
dmoon <dmoon.t@gmail.com>
2016-04-15 16:34:36 +0800

    remove production

--- app/config/index.js
+++ app/config/index.js
@@ -2,7 +2,7 @@ export default {
   // envMode: 'dev',
   // serverDomain: 'http://localhost:1337',
   // socketDomain: 'localhost:1337',
-  envMode: 'production',
+  // envMode: 'production',
   serverDomain: 'http://qa.trademuch.co.uk',
   socketDomain: 'qa.trademuch.co.uk:1337',
 };
