b91e5f2bfa5d7ca20a6c19860deeafea5787e279
dmoon <dmoon.t@gmail.com>
2016-04-18 16:08:44 +0800

    add view of MessageBoard

---
+++ app/containers/MessageBoard.js
@@ -0,0 +1,207 @@
+import React, {
+  NativeModules,
+  Dimensions,
+  View,
+  Component,
+  ListView,
+  Alert,
+  Text,
+  SegmentedControlIOS,
+} from 'react-native';
+import InfiniteScrollView from 'react-native-infinite-scroll-view';
+import * as color from '../style/color';
+import { connect } from 'react-redux';
+import { Actions } from 'react-native-router-flux';
+import ListItem from '../components/PostList/ListItem';
+import ActionButton from '../components/ActionButton';
+import config from '../config/index';
+// import SearchBar from '../components/SearchBar';
+import SearchBar from 'react-native-search-bar';
+import NotificationCircle from '../components/NotificationCount';
+const {
+  RNSearchBarManager,
+} = NativeModules;
+import {
+  requestSearchLoadMore,
+  requestSearchPost,
+  requestSearchPostNextPage,
+} from '../actions/SearchPostActions';
+import { requestSetLocation } from '../actions/GeoActions';
+
+const windowSize = Dimensions.get('window');
+const styles = React.StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 20,
+    backgroundColor: color.MAIN_BACKGROUND_COLOR,
+  },
+  header: {
+    justifyContent: 'center',
+    padding: 10,
+  },
+});
+
+
+export default class MessageBoard extends Component {
+  constructor(props) {
+    super(props);
+    this.getListItem = this.getListItem.bind(this);
+    const dataSource = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.state = {
+      dataSource,
+      showsCancelButton: false,
+    };
+  }
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.props.requestSetLocation(position);
+        this.props.requestSearchLoadMore(false);
+        this.props.requestSearchPost(null, '300km', {
+          lat: position.coords.latitude,
+          lon: position.coords.longitude,
+        });
+      },
+      (error) => Alert.alert(error.message),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.postList !== this.props.postList) {
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(nextProps.postList),
+      });
+    }
+  }
+
+  onChangeText = (value) => {
+    const { location } = this.props;
+    this.props.requestSearchLoadMore(false);
+    this.props.requestSearchPost(value, '60000km', {
+      lat: location.latitude,
+      lon: location.longitude,
+    }, this.props.postList.length);
+  }
+
+  onListItemPress = (id) => {
+    this.handleSearchCancelPress();
+    Actions.postDetail({ id });
+  }
+
+  getListItem(rowData, sectionID, rowID, highlightRow) {
+    let bakColor = {};
+    if (rowID % 2 === 0) {
+      bakColor = { backgroundColor: color.LIST_ITEM_COLOR1 };
+    } else {
+      bakColor = { backgroundColor: color.LIST_ITEM_COLOR2 };
+    }
+    let distance = '';
+    if (rowData.distance !== -1) {
+      if (rowData.distance <= 1) {
+        distance = `${rowData.distance * 1000} m`;
+      } else {
+        distance = `${rowData.distance} km`;
+      }
+    }
+    return (
+      <ListItem
+        id={rowData.id}
+        index={rowData.index}
+        title={rowData.title}
+        img={`${config.serverDomain}${rowData.pic}`}
+        description={distance}
+        onItemPress={this.onListItemPress}
+        bakColor={bakColor}
+        notificationCount={101}
+      />
+    );
+  }
+
+  handleSearchButtonPress = () => {
+    this.searchBarDissmissKeyBoard();
+  }
+
+  searchBarDissmissKeyBoard = () => {
+    RNSearchBarManager.blur(React.findNodeHandle(this.refs.postSearchBar));
+  }
+
+  loadMorePost = () => {
+    const { postList, lastSeachApi } = this.props;
+    this.props.requestSearchLoadMore(false);
+    this.props.requestSearchPostNextPage(lastSeachApi, postList.length);
+  }
+
+  handleSearchCancelPress = () => {
+    this.searchBarDissmissKeyBoard();
+    this.setState({ showsCancelButton: false });
+  }
+
+  handleSearchBarOnFocus = () => {
+    this.setState({ showsCancelButton: true });
+  }
+
+  handleActionButtonPress = () => {
+    Actions.createPost.call();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <SegmentedControlIOS
+            values={['全部', '已讀', '未讀']}
+            selectedIndex={0}
+            tintColor={color.SEGMENTED_CONTROL_SELECTED}
+          />
+        </View>
+        <ListView
+          keyboardDismissMode="on-drag"
+          renderScrollComponent={props => <InfiniteScrollView {...props} />}
+          dataSource={this.state.dataSource}
+          renderRow={this.getListItem}
+          onLoadMoreAsync={this.loadMorePost}
+          canLoadMore={this.props.canLoadMore}
+        />
+    </View>
+    );
+  }
+}
+
+MessageBoard.propTypes = {
+  postList: React.PropTypes.array,
+  location: React.PropTypes.object,
+  lastSeachApi: React.PropTypes.string,
+  canLoadMore: React.PropTypes.bool,
+  requestSearchLoadMore: React.PropTypes.func,
+  requestSearchPost: React.PropTypes.func,
+  onListItemPress: React.PropTypes.func,
+  requestSetLocation: React.PropTypes.func,
+  requestSearchPostNextPage: React.PropTypes.func,
+};
+
+MessageBoard.defaultProps = {
+  postList: [],
+  location: {
+    latitude: 24.148657699999998,
+    longitude: 120.67413979999999,
+  },
+  canLoadMore: true,
+};
+
+function _injectPropsFromStore(state) {
+  return {
+    postList: state.search.postList,
+    lastSeachApi: state.search.lastSeachApi,
+    canLoadMore: state.search.canLoadMore,
+    location: state.geo.location,
+  };
+}
+
+const _injectPropsFormActions = {
+  requestSearchLoadMore,
+  requestSearchPost,
+  requestSetLocation,
+  requestSearchPostNextPage,
+};
+
+export default connect(_injectPropsFromStore, _injectPropsFormActions)(MessageBoard);
