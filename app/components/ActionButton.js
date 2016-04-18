2468750b1f1bc4c32ea46a9428d9100dc2670043
dmoon <dmoon.t@gmail.com>
2016-04-18 17:49:49 +0800

    fixed action button style when landscape

--- app/components/ActionButton.js
+++ app/components/ActionButton.js
@@ -8,7 +8,13 @@ import React, {
   Dimensions,
   PropTypes,
 } from 'react-native';
-const windowSize = Dimensions.get('window');
+let windowSize = Dimensions.get('window');
+const isShort = windowSize.width < windowSize.height;
+windowSize = {
+  width: isShort ? windowSize.width : windowSize.height,
+  height: isShort ? windowSize.width : windowSize.height,
+};
+
 const PIXEL_RATIO = PixelRatio.get();
 const styles = StyleSheet.create({
   icon: {
@@ -32,8 +38,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   contentBlock: {
-    width: windowSize.width,
     flex: 1,
+    width: windowSize.width,
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
