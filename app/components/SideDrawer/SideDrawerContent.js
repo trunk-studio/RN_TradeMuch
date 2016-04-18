7115af2c2678ddbe7ca22248605314f88bc40e86
dmoon <dmoon.t@gmail.com>
2016-04-18 16:10:00 +0800

    add messageBoard route

--- app/components/SideDrawer/SideDrawerContent.js
+++ app/components/SideDrawer/SideDrawerContent.js
@@ -149,7 +149,7 @@ export default class SideDrawerContent extends Component {
           <MenuItem id="postList" title="附近的好康物品" img="http://i.imgur.com/OKrJ2m3.png" notification="" onItemPress={this.onItemPress} />
           <MenuItem id="postList" title="我撿的資源" img="http://i.imgur.com/gwzwb5F.png" notification="" onItemPress={this.onItemPress} />
           <MenuItem id="postList" title="我追蹤的資源" img="http://i.imgur.com/v8iXJJP.png" notification="" onItemPress={this.onItemPress} />
-          <MenuItem id="messenger" title="我的留言板" img="http://i.imgur.com/NBbuVv3.png  " notification="" onItemPress={this.onItemPress} />
+          <MenuItem id="messageBoard" title="我的留言板" img="http://i.imgur.com/NBbuVv3.png  " notification="" onItemPress={this.onItemPress} />
           <MenuItem id="postList" title="我的倉庫" img="http://i.imgur.com/YHOYSAa.png" notification="" onItemPress={this.onItemPress} />
           <MenuItem id="postList" title="尋寶去" img="http://i.imgur.com/dGhdv4x.png" notification="" onItemPress={this.onItemPress} />
           <MenuItem id="login" title={loginBtnTitle} img="http://i.imgur.com/UDw6ykK.png" notification="" onItemPress={this.onItemPress} />
