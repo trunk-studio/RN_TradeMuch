320a7ba6207d369e171f7ba2109f80ed508df096
dmoon <dmoon.t@gmail.com>
2016-04-18 18:27:49 +0800

    merge conflict

--- app/routes.js
+++ app/routes.js
@@ -232,16 +232,12 @@ export default class AppRoutes extends Component {
               <Route schema="none" name="policies" component={Policies} title="服務條款" />
               <Route name="profile" component={Profile} schema="interior" title="個人資料" />
               <Route name="nearByPosts" component={NearByPosts} schema="interior" title="附近好康" />
-<<<<<<< HEAD
               <Route name="messageBoard" schema="home" component={MessageBoard} title="我的留言板" />
-=======
               <Route name="tradeRecord" component={TradeRecord} schema="interior" title="我撿的資源" />
               <Route name="favoriteList" component={FavoriteList} schema="interior" title="我追蹤的資源" />
               <Route name="myItems" component={MyItems} schema="interior" title="我的倉庫" />
               <Route name="category" component={Category} schema="interior" title="尋寶去" />
->>>>>>> 9181f541b3b9c9d4a526a023c2b81ae91833a844
               <Route name="messenger" component={Messenger} schema="interior" title="Messenger" />
-
             </Router>
           </SideDrawer>
         </Route>
