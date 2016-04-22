e24993d85f2853497e074ce06db50f92696a1f22
dmoon <dmoon.t@gmail.com>
2016-04-22 14:47:22 +0800

    add default config

---
+++ app/config/index.default.js
@@ -0,0 +1,8 @@
+export default {
+  envMode: 'dev',
+  serverDomain: 'http://localhost:1337',
+  socketDomain: 'localhost:1337',
+  // envMode: 'production',
+  // serverDomain: 'http://qa.trademuch.co.uk',
+  // socketDomain: 'qa.trademuch.co.uk:1337',
+};
