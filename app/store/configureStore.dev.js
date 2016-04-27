e0d1c8381d6867271ffcaf424bde1cb8c450c8fa
dmoon <dmoon.t@gmail.com>
2016-04-27 15:45:42 +0800

    add redux-logger action exclude list

--- app/store/configureStore.dev.js
+++ app/store/configureStore.dev.js
@@ -4,10 +4,18 @@ import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
 import promise from 'redux-promise';
 import createDevTools from 'remote-redux-devtools';
-
 import rootReducer from '../reducers';
 
-const logger = createLogger();
+import { SWITCH_MINIMAL_UI_MODE } from '../actions/UIStatusActions';
+
+const actionExcludeList = [
+  SWITCH_MINIMAL_UI_MODE,
+];
+
+const logger = createLogger({
+  // options
+  predicate: (getState, action) => actionExcludeList.indexOf(action.type) !== -1,
+});
 // const devTools = createDevTools({
 //   name: Platform.OS,
 //   hostname: 'localhost',
