e5662ba1da584869c8e0e635b58cd7f7e1289006
Kuyen <iamcxa@gmail.com>
2016-05-25 12:25:16 +0800

    fix messanger invaildate

--- app/containers/Messenger.js
+++ app/containers/Messenger.js
@@ -59,7 +59,8 @@ async function getChatHistory(socket, chatRoomId) {
     const request = await composeRequestWithAuthToken(url);
     return new Promise((resolve) => {
       socket.emit('get', request, (response) => {
-        resolve(response.body.result);
+        const result = response.body.result;
+        resolve(result);
       });
     });
   } catch (e) {
@@ -123,8 +124,6 @@ export default class Messenger extends Component {
       });
     }
   }
-
-
 
   messengerRef(c) {
     this._GiftedMessenger = c;
